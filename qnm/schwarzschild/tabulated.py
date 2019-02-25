397d1c0ff2b4d2038c909d29774f77248a8a8e20
Leo C. Stein <leo.stein@gmail.com>
2019-02-24 18:07:17 -0600

    New version of QNMDict, QNMDict2.  Extends existing sequences.

--- qnm/schwarzschild/tabulated.py
+++ qnm/schwarzschild/tabulated.py
@@ -155,3 +155,138 @@ class QNMDict(object):
                 logging.warn("Could not write Schw QNM dict to file {}".format(dict_pickle_file))
 
         return self.qnm_dict
+
+############################################################
+
+class QNMDict2(object):
+    """ TODO Documentation! """
+
+    # Borg pattern, the QNM table will be shared among all instances
+    _shared_state = {}
+
+    def __init__(self, init=False, dict_pickle_file=None):
+
+        self.__dict__ = self._shared_state
+
+        if (not hasattr(self, 'seq_dict')):
+            # First!
+            self.seq_dict = {}
+
+        if (init):
+            self.load_dict(dict_pickle_file=dict_pickle_file)
+
+
+    def default_pickle_file(self):
+        """Give the default path of the QNM dict pickle file, <dirname of this file>/data/Schw_dict.pickle."""
+
+        return os.path.abspath(
+            '{}/data/Schw_dict.pickle'.format(
+                os.path.dirname(os.path.realpath(__file__))))
+
+    def load_dict(self, dict_pickle_file=None):
+        """ Load a Schw QNM dict from disk.
+
+        Params
+        ------
+        dict_pickle_file: string [default: from default_pickle_file()]
+          Filename for reading (or writing) dict of Schwarzschild QNMs
+
+        Returns
+        -------
+        None
+        """
+
+        if (dict_pickle_file is None):
+            dict_pickle_file = self.default_pickle_file()
+
+        try:
+            with open(dict_pickle_file, 'rb') as handle:
+                logging.info("Loading Schw QNM dict from file {}".format(dict_pickle_file))
+                self.seq_dict = pickle.load(handle, encoding='latin1')
+        except:
+            logging.info("Could not load Schw QNM dict from file {}".format(dict_pickle_file))
+
+        return
+
+    def write_dict(self, dict_pickle_file=None):
+        """Write the current state of the QNM dict to disk.
+
+        TODO
+
+        Params
+        ------
+        dict_pickle_file: string [default: from default_pickle_file()]
+          Filename for reading (or writing) dict of Schwarzschild QNMs
+
+        Returns
+        -------
+        None
+        """
+
+        if (dict_pickle_file is None):
+            dict_pickle_file = self.default_pickle_file()
+
+        _the_dir = os.path.dirname(dict_pickle_file)
+        if not os.path.exists(_the_dir):
+            try:
+                os.mkdir(_the_dir)
+            except:
+                logging.warn("Could not create dir {} to store Schw QNM dict".format(_the_dir))
+
+        try:
+            with open(dict_pickle_file, 'wb') as handle:
+                logging.info("Writing Schw QNM dict to file {}".format(dict_pickle_file))
+                pickle.dump(self.seq_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        except:
+            logging.warn("Could not write Schw QNM dict to file {}".format(dict_pickle_file))
+
+        return
+
+    def __call__(self, s, l, n):
+        """Get the Schwarzschild QNM labeled by (s,l,n).
+
+        If the QNM has already been computed, immediately return that
+        value.  If the (s,l) sequence is in the dict, but n has not
+        been computed, extend the sequence to n and return the QNM.
+        If the (s,l) sequence is not already in the dict, add it to
+        the dict out to overtone n.
+
+        Parameters
+        ----------
+        s: int
+          Spin weight of the field.
+
+        l: int
+          Multipole number of the QNM.
+
+        n: int
+          Overtone number of the QNM.
+
+        Returns
+        -------
+        (complex, double, int)
+          The complex value is the QNM frequency.  The double is the
+          estimated truncation error for the continued fraction.  The
+          int is the depth of the continued fraction evaluation.
+
+        Raises
+        ------
+        TODO
+
+        """
+
+        # TODO Check values of (s,l)
+        s = -np.abs(s)
+
+        if ((s,l) not in self.seq_dict.keys()):
+            self.seq_dict[(s,l)] = SchwOvertoneSeq(s=s, l=l, n_max=n)
+
+        seq = self.seq_dict[(s,l)]
+
+        # This is idempotent, no danger in "extending" if the data
+        # have already been computed. Go to n+2 to try to guard
+        # against skipping an overtone in the sequence.
+        # This can raise an exception.
+        seq.extend(n_max=n+2)
+
+        return (seq.omega[n], seq.cf_err[n], int(seq.n_frac[n]))
