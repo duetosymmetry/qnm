22ef9c189302da3541ea22831899f8d402132564
Leo C. Stein <leo.stein@gmail.com>
2019-04-08 23:08:57 -0500

    Avoid UnicodeDecodeError on windows (thanks Vijay!)

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ Standard setup.py to upload the code on pypi.
 '''
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode("UTF-8")
 
 import sys
 sys.path.append("qnm")
