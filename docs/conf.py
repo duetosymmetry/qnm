2878298912a34238ddfa36938157df54895994f8
Leo C. Stein <leo.stein@gmail.com>
2019-07-18 10:26:01 -0500

    Verify with google webmaster tools

--- docs/conf.py
+++ docs/conf.py
@@ -125,6 +125,9 @@ language = None
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# LCS ADDED 18 Jul 2019, additional files to include, but not parse
+html_extra_path = ['googlecd1cadaf1701e6cc.html']
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
